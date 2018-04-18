04c5529e0e7b630953c4619668345d625836c2b4
tmc <1370650+khast3x@users.noreply.github.com>
2018-04-18 02:14:11 +0200

    fixed trevor

--- merge_custom.py
+++ merge_custom.py
@@ -1,18 +1,23 @@
 import json
 import os
+
+
 loaded_templates = []
 localpath = os.getcwd() + "/custom/"
 for filename in os.listdir(localpath):
     print("* found {}".format(filename))
     custom_file = open(localpath + filename)
     s = json.load(custom_file)
-    loaded_templates.append(s)
+    loaded_templates.extend(s)
     custom_file.close()
-print("Generating merged JSON Portainer template")
-for t in loaded_templates:
-    for j in range(len(t)):
-        print("* adding {}".format(t[j]["name"]))
+
+# print(loaded_templates)
+print("+ Generating merged JSON Portainer template")
+
 merged = open("merged_portainer_templates.yml", "w")
+
+for t in loaded_templates:
+    print("* {} added".format(t["name"]))
 merged.write(json.dumps(loaded_templates, indent=2))
-print("✔️ done")
+print("\n✔️ done")
 
